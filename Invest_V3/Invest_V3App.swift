1fe8580b7759ece03017263acc2dd790ecd1e875
WyattLin001 <f227006900.en13@gmail.com>
2025-07-09 23:35:02 +0800

    Initial Commit

---
+++ Invest_V3/Invest_V3App.swift
@@ -0,0 +1,17 @@
+//
+//  Invest_V3App.swift
+//  Invest_V3
+//
+//  Created by 林家麒 on 2025/7/9.
+//
+
+import SwiftUI
+
+@main
+struct Invest_V3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
